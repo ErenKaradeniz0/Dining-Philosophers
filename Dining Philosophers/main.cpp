9ae35c74dbfdb8ad77fef444a0a1d117d1f1df32
Ulaş Deniz Çakmazel <37212344+UlasDenizCakmazel@users.noreply.github.com>
2024-11-27 15:31:05 +0300

    Running Version

--- Dining Philosophers/main.cpp
+++ Dining Philosophers/main.cpp
@@ -10,7 +10,13 @@ int F1;
 enum PhilosophersState {
     THINKING,
     HUNGRY,
-    EATING
+    EATING,
+    DEATH
+};
+
+enum ChopstickState {
+    USING,
+    IDLE
 };
 
 struct Philosopher {
@@ -19,6 +25,15 @@ struct Philosopher {
     int id;
     int x;
     int y;
+    int health;
+    int left_cop; //Chopstick
+    int right_cop; //Chopstick
+};
+
+
+struct Chopstick {
+    ChopstickState state;
+    int x, y;
 };
 
 int PhilosopherID;
@@ -39,6 +54,8 @@ int constant = 5;
 
 char philosopherLabel[4];
 char chopstickLabel[4];
+
+Chopstick chopsticks[CHOPSTICK_LIMIT];
 
 void ICGUI_Create() {
     ICG_MWTitle("Dining Philosophers");
@@ -71,9 +88,9 @@ void CalculateCoordinateChopsticks() {
         int y = centerY + (radius - (4 * constant)) * sin(chopstickAngles[i] * PI / 180);
         FillEllipse(screenMatrix, x, y, 4 * constant, 4 * constant, 0x00FF00); // Chopsticks (green)
         // Generate chopstick label
-        char chopstickLabel[4];
         ChopsticksCoordinate[i][0] = x;
         ChopsticksCoordinate[i][1] = y;
+        chopsticks[i] = Chopstick{ IDLE, x, y };
         PrintNumbertoScreen(chopstickLabel, "CH", i);
         Impress12x20(screenMatrix, x + 1 * constant, y + 2 * constant, chopstickLabel, 0xFFFFFF); // Print ID
     }
@@ -89,13 +106,31 @@ void DrawDefaultImages() {
         Impress12x20(screenMatrix, PhilosophersCoordinate[i][0] + 5 * constant, PhilosophersCoordinate[i][1] + 7 * constant, philosopherLabel, 0xFFFFFF); // Print ID
 
        
-        FillEllipse(screenMatrix, ChopsticksCoordinate[i][0], ChopsticksCoordinate[i][1], 4 * constant, 4 * constant, 0x00FF00); // Chopsticks (green)
+        FillEllipse(screenMatrix, chopsticks[i].x, chopsticks[i].y, 4 * constant, 4 * constant, 0x00FF00); // Chopsticks (green)
         // Generate chopstick label
         PrintNumbertoScreen(chopstickLabel, "CH", i);
-        Impress12x20(screenMatrix, ChopsticksCoordinate[i][0] + 1 * constant, ChopsticksCoordinate[i][1] + 2 * constant, chopstickLabel, 0xFFFFFF); // Print ID
+        Impress12x20(screenMatrix, chopsticks[i].x + 1 * constant, chopsticks[i].y + 2 * constant, chopstickLabel, 0xFFFFFF); // Print ID
 
     }
 
+}
+
+void ControlChopstick() {
+    while(1){
+        for (int i = 0; i < CHOPSTICK_LIMIT; i++) {
+            if (chopsticks[i].state == IDLE) {
+                FillEllipse(screenMatrix, chopsticks[i].x, chopsticks[i].y, 4 * constant, 4 * constant, 0x00FF00); // Chopsticks (green)
+            }
+            else if (chopsticks[i].state == USING) {
+                FillEllipse(screenMatrix, chopsticks[i].x, chopsticks[i].y,
+                    4 * constant, 4 * constant, 0x999999); // Chopsticks (green)
+            }
+            // Generate chopstick label
+            PrintNumbertoScreen(chopstickLabel, "CH", i);
+            Impress12x20(screenMatrix, chopsticks[i].x + 1 * constant, chopsticks[i].y + 2 * constant, chopstickLabel, 0xFFFFFF); //
+        }
+        Sleep(20);
+    }
 }
 
 // Drawing thread
@@ -112,65 +147,144 @@ void DrawThread() {
 
 
 void *PhilosopherThread(LPVOID vv) {
-
     int* tmp = (int*)vv;
     int id;
     id = *tmp;
 
-    int c_id; //Chopstick ID
+    //int count = 0;
+    int sleepy;
 
-    Philosopher p = Philosopher{ THINKING, 0, id, PhilosophersCoordinate[id][0], PhilosophersCoordinate[id][1] } ;
+    Philosopher p = Philosopher{ THINKING, 0, id, PhilosophersCoordinate[id][0], PhilosophersCoordinate[id][1], 25000, id, (id + (PHILOSOPHER_LIMIT - 1)) % PHILOSOPHER_LIMIT } ;
 
-    Sleep(1000); 
+    int using_chopstick; // Using one chopstick but death philosopher, chopstick setting again useablity 
+
 
     //Philosopher p = { THINKING, 0  };
 
-    p.state = HUNGRY; //HUNGRRY
+    //p.state = HUNGRY; //HUNGRRY
 
+    sleepy = rand() % (100 + 1 - 0) + 30;
+
+    Sleep(sleepy);
+
+   
     while(1){
+
+        sleepy = rand() % (100 + 1 - 0) + 30;
+
         switch (p.state)
         {
+            case THINKING: {
+                FillEllipse(screenMatrix, p.x, p.y, 8 * constant, 8 * constant, 0x0000FF); // Philosophers (blue)
+                // Generate philosopher label
+                PrintNumbertoScreen(philosopherLabel, "PL", p.id);
+                Impress12x20(screenMatrix, p.x + 5 * constant, p.y + 7 * constant, philosopherLabel, 0xFFFFFF); // Print ID
+                Sleep(sleepy);
+                p.state = HUNGRY;
+                break;
+            }
             case HUNGRY:{
                 FillEllipse(screenMatrix, p.x, p.y, 8 * constant, 8 * constant, 0xFF0000); // Philosophers (red)
                 PrintNumbertoScreen(philosopherLabel, "PL", p.id);
                 Impress12x20(screenMatrix, p.x + 5 * constant, p.y + 7 * constant, philosopherLabel, 0xFFFFFF);
+                p.health--;
                 break;
             }
             case EATING: {
-                //FillEllipse(screenMatrix, p.x, p.y, 8 * constant, 8 * constant, 0xFFFF00); // Philosophers (yellow)
-                Sleep(1000); //Eating Animation;
-                p.state = THINKING;
+                FillEllipse(screenMatrix, p.x, p.y, 8 * constant, 8 * constant, 0xFFFF00); // Philosophers (yellow)
+                PrintNumbertoScreen(philosopherLabel, "PL", p.id);
+                Impress12x20(screenMatrix, p.x + 5 * constant, p.y + 7 * constant, philosopherLabel, 0xFFFFFF);
+                p.health++;
+                Sleep(sleepy);
+
+                    Line(screenMatrix, p.x + 20, p.y + 30, chopsticks[p.left_cop].x + 20,
+                        chopsticks[p.left_cop].y + 20, 0x000000);
+
+                    Line(screenMatrix, p.x + 20, p.y + 30, chopsticks[p.right_cop].x + 20,
+                        chopsticks[p.right_cop].y + 20, 0x000000);
+                    
+                    p.chopstick = 0;
+                    ChopstickCount += 2;
+                    p.state = THINKING;
+                    
+                    chopsticks[p.left_cop].state = IDLE;
+                    chopsticks[p.right_cop].state = IDLE;
+
+                    
+
+                   
+                break;
+                
+            }
+            case DEATH: {
+                
+                if (p.chopstick == 1) {
+                    chopsticks[using_chopstick].state = IDLE;
+
+                    Line(screenMatrix, p.x + 20, p.y + 30, chopsticks[using_chopstick].x + 20,
+                        chopsticks[using_chopstick].y + 20, 0x000000);
+
+                }
+
+                FillEllipse(screenMatrix, p.x, p.y, 8 * constant, 8 * constant, 0x444444); // Philosophers (yellow)
+                PrintNumbertoScreen(philosopherLabel, "PL", p.id);
+                Impress12x20(screenMatrix, p.x + 5 * constant, p.y + 7 * constant, philosopherLabel, 0xFFFFFF);
+
+
+
+                return 0;
                 break;
             }
-            default: {
+                
+            default :{
                 //FillEllipse(screenMatrix, p.x, p.y, 8 * constant, 8 * constant, 0x0000FF); // Philosophers (yellow)
                 ChopstickCount += 2;
                 p.chopstick = 0;
                 return 0;
+                break;
             }
         }
-        p.chopstick++;
-        ChopstickCount--;
-        if (ChopstickCount >= 0) {
-            c_id = CHOPSTICK_LIMIT - (ChopstickCount + 1);
-            Line(screenMatrix,p.x + 20 ,p.y + 30 , ChopsticksCoordinate[c_id][0] + 20, 
-                ChopsticksCoordinate[c_id][1] + 20 ,0xDDDDDD);
-
-            
-            FillEllipse(screenMatrix, ChopsticksCoordinate[c_id][0],ChopsticksCoordinate[c_id][1],
-                4 * constant, 4 *constant, 0x999999); // Chopsticks (green)
-
-            PrintNumbertoScreen(chopstickLabel, "CH", c_id);
-
-
-            Impress12x20(screenMatrix, ChopsticksCoordinate[c_id][0] + 1 * constant,
-                ChopsticksCoordinate[c_id][1] + 2 * constant, chopstickLabel, 0xFFFFFF); // Print ID
+      
+        if(p.state==HUNGRY){
+            if (p.health == 0) {
+                p.state = DEATH;
+            }
 
             if (p.chopstick == 2) {
-                p.state = HUNGRY;
+                p.state = EATING;
             }
+            else {
+                if (ChopstickCount > 0 && p.chopstick < 2)
+                {
+                    if (chopsticks[p.left_cop].state == IDLE) {
+                        chopsticks[p.left_cop].state = USING;
+                        using_chopstick = p.left_cop;
+                        
+                        p.chopstick++;
+                        ChopstickCount--;
+                        Line(screenMatrix, p.x + 20, p.y + 30, chopsticks[p.left_cop].x + 20,
+                            chopsticks[p.left_cop].y + 20, 0x999999);
+
+                    }
+                    if (chopsticks[p.right_cop].state == IDLE) {
+                        chopsticks[p.right_cop].state = USING;
+                        p.chopstick++;
+                        ChopstickCount--;
+                        Line(screenMatrix, p.x + 20, p.y + 30, chopsticks[p.right_cop].x + 20,
+                            chopsticks[p.right_cop].y + 20, 0x999999);
+                        using_chopstick = p.right_cop;
+
+
+                    }
+
+
+                }
+            }
+
+
         }
-        Sleep(1000);
+        
+        
     }
         
 }
@@ -192,9 +306,10 @@ void StartNonSemaphore(void) {
         //Philosopher  p = { THINKING, 0, id, PhilosophersCoordinate[id][0], PhilosophersCoordinate[id][1] };
         //philosopher[i] = p;
         philosopher[i]=i;
-        CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)PhilosopherThread, &philosopher[i], 0, &ThreadID);
+        CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)PhilosopherThread, (LPVOID)&philosopher[i], 0, &ThreadID);
         Sleep(1);
     }
+    CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)ControlChopstick, NULL, 0, NULL);
     
     CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)DrawThread, NULL, 0, NULL);
  
